8665d1a4154acc57164b28c82859274ac3e8b0e1
Adetayo <akinsanyaadetayo7@gmail.com>
2021-03-25 09:18:21 +0100

    Implement shipping screen with steps

--- src/Screens/ShippingScreen.jsx
+++ src/Screens/ShippingScreen.jsx
@@ -3,16 +3,17 @@ import { Button, Form } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../Components/FormContainer";
 import {saveShippingAddress} from '../actions/cartActions'
+import CheckoutSteps from "../Components/CheckoutSteps";
 
 const ShippingScreen = ({ history }) => {
 
 const cart = useSelector(state => state.cart)
 const {shippingAddress} = cart
 
-  const [address, setAddress] = useState(shippingAddress.address);
-  const [city, setCity] = useState(shippingAddress.city);
-  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode);
-  const [country, setCountry] = useState(shippingAddress.country);
+  const [address, setAddress] = useState(shippingAddress?.address);
+  const [city, setCity] = useState(shippingAddress?.city);
+  const [postalCode, setPostalCode] = useState(shippingAddress?.postalCode);
+  const [country, setCountry] = useState(shippingAddress?.country);
 
   const dispatch = useDispatch()
 
@@ -24,6 +25,7 @@ const {shippingAddress} = cart
   }
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address">
